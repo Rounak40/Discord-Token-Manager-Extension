8e0cc3d49b566bcd1b11e01eda84b02df23a836b
Rounak <50997575+Rounak40@users.noreply.github.com>
2023-12-20 16:26:15 +0530

    Improved Workings !
    Updates:
    1. Improved the user lookup API , as it was giving error sometimes.
    2. Added a refresh button.
    3. Added the avatar of user profile.
    4. Modified username showing format
    4. Improved text css.

--- popup.js
+++ popup.js
@@ -1,105 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
+
   const tokenInput = document.getElementById("tokenInput");
   const addTokenButton = document.getElementById("addToken");
   const tokenList = document.getElementById("tokenList");
-  const totalTokensElement = document.getElementById("totalTokens"); 
+  const totalTokensElement = document.getElementById("totalTokens");
+  const refreshButton = document.getElementById("refreshButton");
+
+
+
+  function refreshUserDetails() {
+    chrome.storage.sync.get('tokens', function (result) {
+      const existingTokens = result.tokens || [];
+      const freshTokens = [];
+      existingTokens.forEach(storeInfo => {
+        newToken = storeInfo.token;
+        getUserInfo(newToken).then(userInfo => {
+            if (userInfo.global_name == null) {
+              var formattedUsername = `${userInfo.username}#${userInfo.discriminator}`;
+            } else {
+              var formattedUsername = `${userInfo.username}`;
+            }
+            var localUserInfo = {
+                token:newToken,
+                username: formattedUsername,
+                avatarURL: `https://cdn.discordapp.com/avatars/${userInfo.id}/${userInfo.avatar}.webp?size=20`,
+              }
+            
+            if (userInfo.avatar == null) {
+              localUserInfo.avatarURL = `https://discord.com/assets/edda5bb474d4135b4296.png`
+            }
+
+            freshTokens.push(localUserInfo);
+            chrome.storage.sync.set({ 'tokens': freshTokens }, function () {
+              console.log('Data stored in tokens:', freshTokens);
+            });
+          
+          }).catch(error => {
+              console.error("Error getting user info:", error);
+          });
+        
+        });
+
+  
+    })
+    
+    
+  }
+
+
+  refreshButton.addEventListener("click", refreshUserDetails);
+
+
 
   chrome.storage.sync.get("tokens", function (data) {
+    console.log(data);
     if (data.tokens) {
       totalTokensElement.textContent = `Stored Tokens ( ${data.tokens.length} )`;
-      data.tokens.forEach(token => {
-        const decodedUserID = atob(token.split('.')[0]);
-        getUserInfo(decodedUserID)
-          .then(userInfo => {
-            const formattedUsername = `${userInfo.global_name} (${userInfo.tag})`;
-            addTokenToList(token, formattedUsername);
-          })
-          .catch(error => {
-            console.error("Error getting user info:", error);
-            addTokenToList(token, "Unknown User");
-          });
-      });
+      data.tokens.forEach(userInfo => {
+        addUserToList(userInfo);
+        })
+
     }
   });
-
-  addTokenButton.addEventListener("click", function () {
-    const inputTokens = tokenInput.value.trim();
-    if (inputTokens) {
-      const newTokens = inputTokens.split(",").map(token => token.trim());
-      chrome.storage.sync.get("tokens", function (data) {
-        const existingTokens = data.tokens || [];
-        const tokensToAdd = newTokens.filter(newToken => !existingTokens.includes(newToken));
-
-        if (tokensToAdd.length > 0) {
-          const updatedTokens = [...existingTokens, ...tokensToAdd];
-          chrome.storage.sync.set({ tokens: updatedTokens }, function () {
-            tokensToAdd.forEach(newToken => {
-              const decodedUserID = atob(newToken.split('.')[0]);
-              getUserInfo(decodedUserID)
-                .then(userInfo => {
-                  const formattedUsername = `${userInfo.global_name} (${userInfo.tag})`;
-                  addTokenToList(newToken, formattedUsername);
-                })
-                .catch(error => {
-                  console.error("Error getting user info:", error);
-                  addTokenToList(newToken, "Unknown User");
-                });
-            });
-            tokenInput.value = "";
-          });
-        } else {
-          alert("All tokens are already added!");
-        }
-      });
-    }
-  });
-
-  function addTokenToList(token, formattedUsername) {
-    const listItem = document.createElement("li");
-    listItem.classList.add("token-item");
-
-    listItem.textContent = formattedUsername;
-
-    const loginButton = document.createElement("button");
-    loginButton.textContent = "Login";
-    loginButton.addEventListener("click", function () {
-      executeLogin(token);
-    });
-    
-
-    const removeButton = document.createElement("button");
-    removeButton.textContent = "Remove";
-    removeButton.addEventListener("click", function () {
-      const decodedUserID = atob(token.split('.')[0]);
-      chrome.storage.sync.get("tokens", function (data) {
-        const tokens = data.tokens || [];
-        const index = tokens.findIndex(existingToken => {
-          const existingDecodedUserID = atob(existingToken.split('.')[0]);
-          return existingDecodedUserID === decodedUserID;
-        });
-        if (index > -1) {
-          tokens.splice(index, 1);
-          chrome.storage.sync.set({ tokens: tokens }, function () {
-            listItem.remove();
-          });
-        }
-      });
-    });
-
-    listItem.appendChild(loginButton);
-    listItem.appendChild(removeButton);
-    tokenList.appendChild(listItem);
-  }
-
-  function getUserInfo(userID) {
-    const DISCORD_LOOKUP_API_URL = `https://discordlookup.mesavirep.xyz/v1/user/${userID}`;
-    return fetch(DISCORD_LOOKUP_API_URL)
-      .then(response => response.json())
-      .catch(error => {
-        console.error("Error fetching user info:", error);
-        throw error;
-      });
-  }
 
   function executeLogin(token) {
     chrome.tabs.executeScript(null, {
@@ -113,4 +75,149 @@ document.addEventListener("DOMContentLoaded", function () {
       "}; login();"
     });
   }
+
+
+  function getUserInfo(token) {
+    const DISCORD_API_URL = "https://discord.com/api/v10/users/@me";
+  
+    return fetch(DISCORD_API_URL, {
+      headers: {
+        Authorization: token,
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error("Error fetching user info:", error);
+        throw error;
+      });
+  }
+
+ 
+  addTokenButton.addEventListener("click", function () {
+    const inputTokens = tokenInput.value.trim();
+    if (inputTokens) {
+      const newTokens = inputTokens.split(",").map(token => token.trim());
+
+      chrome.storage.sync.get('tokens', function (result) {
+        const existingTokens = result.tokens || [];
+    
+        newTokens.forEach(newToken => {
+
+          const isTokenUnique = !existingTokens.some(item => item.token === newToken);
+      
+          if (isTokenUnique) {
+
+
+            getUserInfo(newToken).then(userInfo => {
+                if (userInfo.global_name == null) {
+                  var formattedUsername = `${userInfo.username}#${userInfo.discriminator}`;
+                } else {
+                  var formattedUsername = `${userInfo.username}`;
+                }
+
+                var localUserInfo = {
+                  token:newToken,
+                  username: formattedUsername,
+                  avatarURL: `https://cdn.discordapp.com/avatars/${userInfo.id}/${userInfo.avatar}.webp?size=256`,
+                }
+
+                if (userInfo.avatar == null) {
+                  localUserInfo.avatarURL = `https://discord.com/assets/edda5bb474d4135b4296.png`
+                }
+
+                existingTokens.push(localUserInfo);
+
+                chrome.storage.sync.set({ 'tokens': existingTokens }, function () {
+                  console.log('Data stored in tokens:', existingTokens);
+                });
+
+                addUserToList(localUserInfo);
+
+              }).catch(error => {
+                console.error("Error getting user info:", error);
+              });
+
+            }
+          else {
+            const duplicateTokenDict = existingTokens.find(item => item.token === newToken);
+            alert(`Token already exists: ${duplicateTokenDict.username} || ${duplicateTokenDict.token}`);
+
+          }
+  
+          
+          });
+        
+        totalTokensElement.textContent = `Stored Tokens ( ${existingTokens.length} )`;
+        
+        tokenInput.value = "";
+
+        
+
+      });
+
+        
+    }
+  });
+
+  function addUserToList(user) {
+
+    const listItem = document.createElement("li");
+    listItem.classList.add("token-item");
+
+    const avatarDiv = document.createElement("div");
+    avatarDiv.classList.add("avatar-container");
+
+    const avatarImage = document.createElement("img");
+    avatarImage.src = user.avatarURL;
+    avatarImage.classList.add("avatar-image");
+
+    avatarDiv.appendChild(avatarImage);
+
+    listItem.appendChild(avatarDiv);
+
+    listItem.appendChild(document.createTextNode(user.username));
+
+    const loginButton = document.createElement("button");
+    loginButton.textContent = "Login";
+    loginButton.classList.add("login-button");
+    loginButton.addEventListener("click", function () {
+      executeLogin(user.token);
+    });
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", function () {
+
+      chrome.storage.sync.get('tokens', function (result) {
+        const existingTokens = result.tokens || [];
+        const indexToRemove = existingTokens.findIndex(item => item.token === user.token);
+        if (indexToRemove !== -1) {
+          existingTokens.splice(indexToRemove, 1);
+          chrome.storage.sync.set({ 'tokens': existingTokens }, function () {
+            console.log('Data stored in tokens:', existingTokens);
+          });
+          listItem.remove();
+          totalTokensElement.textContent = `Stored Tokens ( ${existingTokens.length} )`;
+
+        } else {
+          console.log('Token not found in the list.');
+        }
+      });
+
+    });
+
+    listItem.appendChild(loginButton);
+    listItem.appendChild(removeButton);
+    tokenList.appendChild(listItem);
+
+
+  }
+
+
+
 });
